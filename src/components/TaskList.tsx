ba4cdf618d7ff5b800ad33accbd6ae68c577a0e8
joaoJS-Love <joao.vitordeoliveira@hotmail.com>
2021-12-02 20:54:05 -0400

    complete-challenge

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -14,16 +14,38 @@ export function TaskList() {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
+  function randomID(min=1, max=10000) {
+    return Math.floor(Math.random()*(max-min+1)+min);
+  }
+
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (newTaskTitle?.trim() === '') return;
+    
+    const newTask = {
+      id: randomID(),
+      title: newTaskTitle,
+      isComplete: false
+    }
+
+    setTasks([...tasks, newTask]);
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const tasksChanged = tasks.map(task => {
+      if (task.id === id) task.isComplete = !task.isComplete;
+      return task;
+    });
+
+    setTasks(tasksChanged);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const tasksChanged = tasks.filter(task => task.id !== id);
+    setTasks(tasksChanged);
   }
 
   return (
